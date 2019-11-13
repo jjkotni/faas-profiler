2196ba3208403f0d7f7dee349b67edd57a51ce89
jjkotni <kjjswaroop@gmail.com>
2019-11-13 11:28:47 +0530

    Script to deploy all pyperf benchmarks created in functions/pyperfbenchmark folder

--- functions/pyperfbenchmark/deploy.sh
+++ functions/pyperfbenchmark/deploy.sh
@@ -1,6 +1,28 @@
-benchmarks=("bm_json_dumps" "bm_deltablue" "bm_mako" "bm_pidigits" "bm_regex_v8")
+#Usage: set FAAS_ROOT as home directory for faas-profiler and run the script to deploy all pyperfbenchmarks
+export WSK_CONFIG_FILE=~/openwhisk-devtools/docker-compose/.wskprops
 
-for i in "${benchmarks[@]}"
+PYPERF_DIR=$FAAS_ROOT/functions/pyperfbenchmark
+BENCHMARKS=()
+
+for ENTRY in "$PYPERF_DIR"/*
 do
-	wsk -i action create $i --kind python:3 $i/$i.zip --web true
+    if [[ $ENTRY == *"bm_"* ]] && [[ $ENTRY != *".zip"* ]] && [[ $ENTRY != *".py"* ]] && [[ $ENTRY != *"virtualenv"* ]]; then
+	BENCHMARK=${ENTRY#"$PYPERF_DIR/"}
+	BENCHMARKS=( "${BENCHMARKS[@]}" "$BENCHMARK" )
+    fi
 done
+
+wsk -i action list > tmp_actions
+FILE_NAME=tmp_actions
+
+for BENCHMARK in "${BENCHMARKS[@]}"
+do
+	if grep -q $BENCHMARK "$FILE_NAME"; then
+	    echo "$BENCHMARK action already invoked!"
+	else
+	    echo "$BENCHMARK"
+            wsk -i action create $BENCHMARK --kind python:3 $PYPERF_DIR/$BENCHMARK/$BENCHMARK.zip --web true
+	fi
+done
+
+rm $FILE_NAME
